ca056c656f649c56bc6bc36217f483c32d00201f
zihan <zyang17@uoguelph.ca>
2023-07-11 14:49:47 -0400

    add header page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect} from 'react';
 import Layout from './compoments/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './compoments/home/Home';
+import Header from './compoments/header/Header';
 
 function App() {
   
@@ -23,6 +24,7 @@ function App() {
 
     return (
         <div className="App">
+            <Header />
             <Routes>
                 <Route path='/' element={<Layout />}>
                     <Route path='/' element={<Home movies={movies} />}></Route>
---
+++ frontend/src/compoments/header/Header.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import { Container } from "@mui/material";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" >
+            <Navbar.Brand className="ms-4" href="/" style={{"color":'gold'}}>
+                <FontAwesomeIcon icon ={faVideoSlash}/>Gold
+            </Navbar.Brand>
+            <Nav className="me-auto my-2 my-lg-0" style={{maxHeight: '100px'}} navbarScroll>
+                <NavLink className ="nav-link" to="/">Home</NavLink>
+                <NavLink className ="nav-link" to="/watchList">Watch List</NavLink>      
+            </Nav>
+            <Button variant="outline-info" className="me-2">Login</Button>
+            <Button variant="outline-info" className="me-4">Register</Button>
+        </Navbar>
+      )
+    }
+
+export default Header
